3e8b619a400051cb8bdf074f53612b4c476a2feb
ViktorBazsi <viktor.balazs.endre@gmail.com>
2024-12-30 01:15:23 +0100

    DONE - landing page ver 001 NEXT STEP - registerForm, first contact with server! gamePage for users

---
+++ Frontend/src/components/LoginForm.jsx
@@ -0,0 +1,85 @@
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { useNavigate } from "react-router-dom";
+import { userValidationSchemaForLogin } from "../schema/userValidationSchema";
+import { Link } from "react-router-dom";
+
+export default function LoginForm() {
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const initialValues = { email: "", password: "" };
+
+  const handleLogin = async (values) => {
+    const result = await login(values);
+
+    if (result.ok) {
+      alert("Sikeres bejelentkezés");
+      navigate("/signedIn");
+    } else {
+      const errorMessage =
+        result.message?.response?.data?.error || "Ismeretlen hiba";
+      const statusCode = result.message?.response?.status || "N/A";
+      alert(`Sikertelen bejelentkezés! ${statusCode} - ${errorMessage}`);
+    }
+  };
+
+  return (
+    <div className="w-full mx-aut my-40 bg-white p-5 rounded-md bg-opacity-35">
+      <h2 className="font-bold text-xk text-white text-xl mb-6">
+        Bejelentkezés
+      </h2>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={userValidationSchemaForLogin}
+        onSubmit={handleLogin}
+      >
+        <Form>
+          <div className="mb-4">
+            <Field
+              type="email"
+              name="email"
+              placeholder="Email cím"
+              className="w-full border p-2 rounded my-1 text-gray-800"
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="text-red-500 text-sm"
+            />
+          </div>
+          <div className="mb-4">
+            <Field
+              type="password"
+              name="password"
+              placeholder="jelszó"
+              className="w-full border p-2 rounded my-1 text-gray-800"
+            />
+            <ErrorMessage
+              name="password"
+              component="div"
+              className="text-red-500 text-sm"
+            />
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="border-2 bg-purple-950 bg-opacity-25 my-5 py-5 px-4 w-3/4 rounded-md text-white text-lg font-bold transform transition duration-700 hover:scale-110 hover:bg-purple-950 hover:bg-opacity-55"
+            >
+              Bejelentkezés
+            </button>
+          </div>
+        </Form>
+      </Formik>
+      <div className="flex justify-center text-white">
+        Nem vagy regisztrálva?
+        <Link
+          to=""
+          className="text-white hover:underline pl-2 hover:scale-110 transition duration-700"
+        >
+          Regisztrálj
+        </Link>
+      </div>
+    </div>
+  );
+}
--- Frontend/src/pages/MainPageLanding.jsx
+++ Frontend/src/pages/MainPageLanding.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
+import LoginForm from "../components/LoginForm";
+
 export default function MainPageLanding() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <div className="h-screen bg-bg1-pattern bg-cover bg-center">
       <div className="h-full flex flex-col justify-end items-center text-white text-center">
-        <button className="border-2 text-white px-4 py-2 rounded-lg hover:scale-110 transition duration-700 mb-48">
+        <button
+          className="border-2 text-white px-4 py-2 rounded-lg hover:scale-110 transition duration-700 mb-48"
+          onClick={toggleModal}
+        >
           BELÉPÉS
         </button>
       </div>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="flex flex-col items-center justify-center bg-white bg-opacity-5 p-6 rounded-lg shadow-lg text-black w-1/2 relative h-3/4">
+            <button
+              onClick={toggleModal}
+              className="absolute top-2 right-2 text-white text-xl font-bold hover:scale-125 transition duration-700 "
+            >
+              ×
+            </button>
+            <LoginForm />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
